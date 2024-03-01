f1c65f4f348b7ef95d40b143c97b5dbdbef800cd
Matheus Maranhão <matheusmaranhao02@gmail.com>
2024-03-01 11:33:12 -0300

    Criando a area de tarefas

--- App.tsx
+++ App.tsx
@@ -1,18 +1,53 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
 
-import Login from './src/components/Login';
+import Login from './src/components/Login'
+import TaskList from './src/components/TaskList';
+
+let listaTask = [
+  { key: '1', nome: 'Passear com o cachorro' },
+  { key: '2', nome: 'Ir no mercado' },
+]
 
 export default function App() {
   const [user, setUser] = useState(null);
 
+  const [newTask, setNewTask] = useState('')
   if (!user) {
     return <Login changeStatus={(user: React.SetStateAction<null>) => setUser(user)} />
   }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>DENTRO DA TELA TAREFAS</Text>
+
+      <View style={styles.containerAdd}>
+        <TextInput
+          style={styles.inputAdd}
+          placeholder='Adicione uma tarefa'
+          value={newTask}
+          onChangeText={(text) => setNewTask(text)} />
+
+        <TouchableOpacity style={styles.buttonAdd}>
+          <Text style={styles.buttonAddText}>+</Text>
+        </TouchableOpacity>
+
+      </View>
+
+      <FlatList
+        data={listaTask}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <TaskList></TaskList>
+      )} />
+
     </SafeAreaView>
   );
 }
@@ -22,6 +57,32 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 25,
     paddingHorizontal: 10,
-    backgroundColor: '#F2f6fc'
-  }
+    backgroundColor: '#f2f6fc'
+  },
+  containerAdd: {
+    flexDirection: 'row'
+  },
+  inputAdd: {
+    flex: 1,
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: '#FFF',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#000000',
+    height: 45,
+  },
+  buttonAdd: {
+    backgroundColor: '#2bbcff',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 6,
+    paddingHorizontal: 19,
+    borderRadius: 4,
+  },
+  buttonAddText: {
+    color: '#FFF',
+    fontSize: 23,
+  },
 })
---
+++ src/components/TaskList/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet
+} from "react-native"
+
+export default function TaskList() {
+    return (
+        <View>
+            <Text>ITEM TAREFAS</Text>
+        </View>
+    )
+}
